181702a87099717ad0516ca316ec99446cfb77bb
moin2672 <moin2672@gmail.com>
2022-12-29 08:56:07 +0530

    0. Outputting dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,18 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 12, 29);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
-    <>
-      <h2>Empense Item!</h2>
-    </>
+    <div className="expense-item">
+      <div>{expenseDate.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
   );
 }
 
