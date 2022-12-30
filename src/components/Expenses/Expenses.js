01e0d0dd43a9646b2a9a541c93544a0f9461f1e7
moin2672 <moin2672@gmail.com>
2022-12-30 17:42:53 +0530

    16. controlled-uncontrolled and stateless-stateful (theory)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,6 +5,55 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpenseFilter";
 
+/*
+CONTROLLED COMPONENT: whenever we are using two way binding, then we are controlling a component
+
+  <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+but here we are controlling our custom component
+it means the values in the ExpensesFilter.js component are passed on to the parent component Expenses.js component => onChangeFilter={filterChangeHandler}
+  <select value={props.selected} onChange={dropdownChangeHandler}>
+and received from parent component => selected={filteredYear}
+
+regarding select field.. real logic resides in parent component
+that turn ExpenseFilter into called CONTROLLED COMPONENT
+
+both the value as well as the changes to the value are not handled in a component itself, but in a parent component
+Expenses.js component controls ExpenseFilter.js component
+
+PRESENTATIONAL VS STATEFUL component
+------------------------------------
+STATELESS VS STATEFUL component
+-------------------------------
+DUMB VS SMART Component
+-----------------------
+Basically all react component in react manages some state.. i.e. useState
+eg:
+Expenses.js 
+const [filteredYear, setFilteredYear]=useState('2020');
+
+ExpenseForm.js
+ const [enteredTitle, setEnteredTitle] = useState('');
+const [enteredAmount, setEnteredAmount] = useState('');
+const [enteredDate, setEnteredDate] = useState('');
+
+there are other components which don't manage any state, then it is called as STATELESS component
+
+ExpenseItem.js
+<Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+
+
+STATELESS / PRESENTATIONAL / DUMB component
+because it does not have internal state, it is available to output some data.
+
+In most react applicaitons there are more STATELESS component than STATEFUL component
+*/
+
 const Expenses = (props) => {
 
   const [filteredYear, setFilteredYear]=useState('2020');
@@ -12,6 +61,7 @@ const Expenses = (props) => {
   const filterChangeHandler = selectedYear =>{
     console.log('Expenses.js')
     console.log(selectedYear)
+    setFilteredYear(selectedYear)
   }
 
   return (
