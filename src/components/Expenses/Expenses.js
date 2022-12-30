18c4e8bd9940dd736efe49dc58042f81b2013ab8
moin2672 <moin2672@gmail.com>
2022-12-30 19:03:14 +0530

    18b. assigning JSX code to a variable and using it as a simple variable - expensesContent

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -67,23 +67,26 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No Expenses Found</p>
+
+  if(filteredExpenses.length>0){
+    expensesContent= filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.length === 0 &&  <p>No Expenses Found</p>}
-      {filteredExpenses.length > 0 &&
-        filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))
-      }
+      {expensesContent}
     </Card>
   );
 };
