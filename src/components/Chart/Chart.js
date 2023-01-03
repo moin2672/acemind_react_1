fb27d237a18b7cb709bbafb19d67aea79fa3cd41
moin2672 <moin2672@gmail.com>
2023-01-03 10:52:38 +0530

    20. adding conditional return statements

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Chart.css'
+import ChartBar from './ChartBar'
+
+function Chart() {
+  return (
+    <div className='chart'>
+      
+    </div>
+  )
+}
+
+export default Chart
